f5b69a85d535d54dc260471801eba2ee8300a628
Paulo Junior <jr@live.at>
2021-09-02 16:48:37 -0300

    Initial Commit

---
+++ CoordinatorsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorsTest
+//
+//  Created by Junior Ferreira on 02/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
